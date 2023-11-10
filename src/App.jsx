e4927379d062758e9e5adf2f83234bb6d8c91943
Rodrigo Alves Souza <121073778+RodFront@users.noreply.github.com>
2023-11-10 02:20:54 +0000

    Inserção e estilização dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ function App() {
     <>
      <div className='card'>
       <h2>Rick Sanchez</h2>
+      <div>
+        <button className='bup'>Status: Vivo</button>
+        <button className='bup'>Espécie: Humana</button>
+      </div>
+       <div>
+        <button className='bdo'>Origem: Terra C-137</button>
+        </div> 
       <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
      </div>
     </>
