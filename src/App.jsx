1e2a50e0dca28dc72d6be8e77de1a96e790f1ca3
Rodrigo Alves Souza <121073778+RodFront@users.noreply.github.com>
2023-11-14 01:12:38 +0000

    aula 2 - implementacao das props do card criacao de components

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ import './App.css'
 import Card from './components/Cards/Card'
 
 function App() {
+  const item1 = {
+  name: "Rick Sanchez",
+  imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+  }
+  const item2 = {
+    name: "Rick Sanchez",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+  }
+
+  
   return (
     <>
-     <div className='card'>
-      <h2>Rick Sanchez</h2>
-      <div>
-        <button className='bup'>Status: Vivo</button>
-        <button className='bup'>Espécie: Humana</button>
-      </div>
-       <div>
-        <button className='bdo'>Origem: Terra C-137</button>
-        </div> 
-      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-     </div>
+    
      <br />
-     <Card />
+     
+     <Card item={item1} />
+     <Card item={item2} />
     </>
   )
 }
--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,3 +1,15 @@
-export default function Card() {
-    return <div>Card</div>
+export default function Card(props) {
+    const item = props.item
+    
+    return  <div className='card'>
+    <h2>{item.name}</h2>
+    <div>
+      <button className='bup'>Status: Vivo</button>
+      <button className='bup'>Espécie: Humana</button>
+    </div>
+     <div>
+      <button className='bdo'>Origem: Terra C-137</button>
+      </div> 
+    <img src= {item.imageUrl} />
+   </div>
 }
